ce77b0d99c41cd551d1d4f10208b2a40b97018b0
refkoer <refkoer@users.noreply.github.com>
2017-12-21 15:32:22 -0500

    nav fix

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-var wrapper=document.querySelector(".super-wrapper-inner");
+// var wrapper=document.querySelector(".super-wrapper-inner");
 
 var listenlink=document.querySelector(".nav-listen a");
 listenlink.addEventListener("click", function(e){
   e.preventDefault();
-  wrapper.classList.add("show-listen")
-  wrapper.classList.remove("show-bio")
-  wrapper.classList.remove("show-contact")
+  document.body.classList.add("show-listen")
+  document.body.classList.remove("show-bio")
+  document.body.classList.remove("show-contact")
 })
 
 var biolink=document.querySelector(".nav-bio a");
 biolink.addEventListener("click", function(e){
   e.preventDefault();
-  wrapper.classList.remove("show-listen")
-  wrapper.classList.add("show-bio")
-  wrapper.classList.remove("show-contact")
+  document.body.classList.remove("show-listen")
+  document.body.classList.add("show-bio")
+  document.body.classList.remove("show-contact")
 })
 
 var contactlink=document.querySelector(".nav-contact a");
 contactlink.addEventListener("click", function(e){
   e.preventDefault();
-  wrapper.classList.remove("show-listen")
-  wrapper.classList.remove("show-bio")
-  wrapper.classList.add("show-contact")
+  document.body.classList.remove("show-listen")
+  document.body.classList.remove("show-bio")
+  document.body.classList.add("show-contact")
 })
